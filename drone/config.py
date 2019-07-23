5e1ae96f27fca37a62599e3355e0aa6946f5cbd4
Hwiyoung <huddy1204@gmail.com>
2019-07-23 10:26:47 +0900

    feat: Add test data

--- drone/config.py
+++ drone/config.py
@@ -1,9 +1,11 @@
 class BaseConfig:
-    LDM_ADDRESS = 'http://61.38.45.117:5000/'
-    MAGO3D_ADDRESS = 'http://61.38.45.117:20080/'
+    #LDM_ADDRESS = 'http://61.38.45.117:5000/'	
+    #MAGO3D_ADDRESS = 'http://61.38.45.117:20080/'
+    LDM_ADDRESS = 'http://192.168.0.13/'	# Myself
+    MAGO3D_ADDRESS = 'http://192.168.0.4/'	# Share computer
     LDM_PROJECT_NAME = 'watchdog'
     DIRECTORY_TO_WATCH = 'drone/downloads'
-    DIRECTORY_IMAGE_CHECK = 'drone/downloads'
+    DIRECTORY_IMAGE_CHECK = 'examples'
     IMAGE_FILE_EXT = 'jpg'
     EO_FILE_EXT = 'txt'
     UPLOAD_INTERVAL = 1
