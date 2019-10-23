211eb4b1249ecfcda45058b5f506bf0de5df4969
hwiyoung <huddy1204@gmail.com>
2019-10-24 01:02:24 +0900

    refactor: Change IP address in website

--- drone/config.py
+++ drone/config.py
@@ -1,5 +1,5 @@
 class BaseConfig:
-    MAGO3D_ADDRESS = 'http://61.38.45.99:20080/'
+    MAGO3D_ADDRESS = 'http://ys.innopam.com:20080/'
     LDM_ADDRESS = 'http://localhost:8080/'  # Local server
     LDM_PROJECT_NAME = 'watchdog'
     DIRECTORY_TO_WATCH = 'drone/downloads'
