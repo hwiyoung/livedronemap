53c1dc85ec22cf8f321d78a2addbff08e69ecc1b
hwiyoung <huddy1204@gmail.com>
2020-04-28 15:27:25 +0900

    test: Demo for Jeju using estimated EOPs

--- config/config_watchdog.py
+++ config/config_watchdog.py
@@ -1,8 +1,8 @@
 class BaseConfig:
     LDM_ADDRESS = 'http://127.0.0.1:30011/'
     MAGO3D_ADDRESS = 'http://ys.innopam.com:20080/'
-    LDM_PROJECT_NAME = '포토스캔 OPK - 사진회전O_RPY'
-    DIRECTORY_IMAGE_CHECK = "/internalCompany/PM2019006_Jeju/191009"
+    LDM_PROJECT_NAME = 'YTN test'
+    DIRECTORY_IMAGE_CHECK = "/internalCompany/PM2019006_Jeju/191009_1"
     DIRECTORY_TO_WATCH = "/mnt/AIMIFY/live_image/abx101/20191129/sony"
     # DIRECTORY_IMAGE_CHECK = 'drone/examples'
     # DIRECTORY_TO_WATCH = 'drone/downloads'
